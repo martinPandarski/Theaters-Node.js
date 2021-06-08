f50166ce62c13be2844c42d590347acc8f458f60
martinPandarski <67786432+martinPandarski@users.noreply.github.com>
2021-06-08 17:05:46 +0300

    update

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    PORT: 4000,
+    PORT: process.env.PORT || 4000,
     DB_URI : 'mongodb+srv://marto65481:REKlFWMFA0OW8TBG@cluster0.4jhxq.mongodb.net/Theaters?retryWrites=true&w=majority',
     SALT_ROUNDS : 10,
     SECRET : 'SALTGENERATOR',
