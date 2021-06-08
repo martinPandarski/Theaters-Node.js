2a4091c87eb4b795b08fbc4e77a28539a3bfa142
martinPandarski <67786432+martinPandarski@users.noreply.github.com>
2021-06-08 16:07:44 +0300

    added mongoAtlas config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
     PORT: 4000,
-    DB_URI : 'mongodb://localhost/Theaters',
+    DB_URI : 'mongodb+srv://marto65481:REKlFWMFA0OW8TBG@cluster0.4jhxq.mongodb.net/Theaters?retryWrites=true&w=majority',
     SALT_ROUNDS : 10,
     SECRET : 'SALTGENERATOR',
     COOKIE_NAME : 'TOKEN'
--- services/playService.js
+++ services/playService.js
@@ -3,58 +3,62 @@ const Play = require('../models/Play');
 
 
 const create = (playData, userId) => {
-   let play = new Play({...playData, createdAt: new Date(), creator: userId});
-   return play.save();
+    let play = new Play({ ...playData, createdAt: new Date(), creator: userId });
+    return play.save();
 }
 
 const getAll = (sortQuery) => {
-   if(sortQuery == 'createdAt'){
-    return Play.find({}).sort({sortQuery: 1}).lean();
-   }else if(sortQuery == 'likedByUsers'){
-       console.log(sortQuery)
-    return Play.find({}).sort({sortQuery: 1}).lean(); 
-   }else{
-       return Play.find({})
-        .sort({createdAt: -1})
+    if (sortQuery == 'createdAt') {
+        
+        return Play.find({}).sort({ sortQuery: 1 }).lean();
+    } else if (sortQuery == 'likedByUsers') {
+   
+        return Play.find({}).sort({ sortQuery: 1 }).lean();
+    }
+
+ 
+
+    return Play.find({})
+        .sort({ createdAt: -1 })
         .lean();
-   }
+
 
 }
 
 const getOne = (id, userId) => {
-     return Play.findById(id)
-     .populate('likedByUsers')
-     .lean()
-     .then(play => {
-             play.likedPlays = play.likedByUsers.some(x => x._id = userId)
-             play.isOwn = play.creator == userId
-             return play
-            }
-         )
-     
+    return Play.findById(id)
+        .populate('likedByUsers')
+        .lean()
+        .then(play => {
+            play.likedPlays = play.likedByUsers.some(x => x._id = userId)
+            play.isOwn = play.creator == userId
+            return play
+        }
+        )
+
 }
 
 const likePlay = (playId, userId) => {
     console.log(userId)
     return Play.findById(playId)
-    .then(play => {
-        play.likedByUsers.push(userId);
+        .then(play => {
+            play.likedByUsers.push(userId);
 
-        return play.save();
-    })
+            return play.save();
+        })
 
 }
 
 const deletePlay = (playId) => {
-   return Play.deleteOne({_id : playId})
+    return Play.deleteOne({ _id: playId })
 }
 
 const updateOne = (playId, playData) => {
-    return Play.updateOne({_id: playId}, playData )
+    return Play.updateOne({ _id: playId }, playData)
 }
 
 const getPopular = (num) => {
-  return  Play.find({}).sort({likedByUsers : -1}).limit(num).lean();
+    return Play.find({}).sort({ likedByUsers: -1 }).limit(num).lean();
 }
 
 module.exports = {
